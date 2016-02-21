c685d2ae380b9baa34c3ebb4b39793500748640e
Allen Krauskopf <alkrauskopf@gmail.com>
2016-02-21 15:51:43 -0700

    gems versions initializers

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
+
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 EscentPartners::Application.initialize!
+
+require File.expand_path('../initializers/mysql2_adapter', __FILE__)
+
---
+++ config/initializers/mysql2_adapter.rb
@@ -0,0 +1,5 @@
+# Be sure to restart your server when you modify this file.
+
+ class ActiveRecord::ConnectionAdapters::Mysql2Adapter
+   NATIVE_DATABASE_TYPES[:primary_key] = "int(11) auto_increment PRIMARY KEY"
+ end

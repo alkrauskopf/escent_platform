dd5018991278fcc1b3bce16995e5f29277b215db
Allen Krauskopf <alkrauskopf@gmail.com>
2016-03-11 19:06:44 -0700

    Initializer fix

--- config/initializers/abstract_mysql_adapter.rb
+++ config/initializers/abstract_mysql_adapter.rb
@@ -1,3 +1,3 @@
-class ActiveRecord::ConnectionAdapters::MysqlAdapter
-  NATIVE_DATABASE_TYPES[:primary_key] = "int(11) auto_increment PRIMARY KEY"
-end
+#class ActiveRecord::ConnectionAdapters::MysqlAdapter
+#  NATIVE_DATABASE_TYPES[:primary_key] = "int(11) auto_increment PRIMARY KEY"
+# end

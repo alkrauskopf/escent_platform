ceae6f893a447f317f089227cb03f5bb0ab6f757
Allen Krauskopf <alkrauskopf@gmail.com>
2016-06-17 17:10:16 -0600

    Rails 3 Initializers

--- config/initializers/requires.rb
+++ config/initializers/requires.rb
@@ -5,3 +5,10 @@ require 'public_persona'
 #require 'calendar_date_select'
 require 'core_ext'      # escent specific core extensions
 require 'prawn'
+
+require './lib/organization_reistration.rb'
+require './lib/authorization_requirement.rb'
+require './lib/finder_helper.rb'
+require './lib/core_ext.rb'
+require './lib/public_persona.rb'
+require './lib/import_excel.rb'

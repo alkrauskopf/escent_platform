ef6dbb693d637edabb22b0e22680c472ed4bbc22
Allen Krauskopf <alkrauskopf@gmail.com>
2016-07-21 14:18:15 -0600

    more tiny_me extractions

--- app/controllers/admin/our_organization_controller.rb
+++ app/controllers/admin/our_organization_controller.rb
@@ -4,16 +4,7 @@ class Admin::OurOrganizationController < Admin::ApplicationController
   include SimpleCaptcha::ControllerHelpers
   
 #  include OrganizationRegistration
-  
-  uses_tiny_mce(:options => {:mode => "textareas",
-    :theme => 'advanced',
-    :theme_advanced_toolbar_location => "top",
-    :theme_advanced_toolbar_align => "left",
-    :theme_advanced_buttons1 => "bold,italic,underline,separator,strikethrough,justifyleft,justifycenter,justifyright, justifyfull,bullist,numlist,undo,redo,separator,spellchecker,code",
-    :theme_advanced_buttons2 => "",
-    :theme_advanced_buttons3 => "",
-    :theme_advanced_resizing => true,
-    :theme_advanced_resize_horizontal => false})
+
 
   before_filter :clear_notification
 

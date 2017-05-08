d72b792c91f94870cf60082070979ec421385b48
Allen Krauskopf <alkrauskopf@gmail.com>
2017-05-08 16:02:37 -0600

    View Benchmarks in IFA Plan Milestones

--- app/helpers/apps/ifa_plan_helper.rb
+++ app/helpers/apps/ifa_plan_helper.rb
@@ -11,4 +11,26 @@ module Apps::IfaPlanHelper
     end
     achieve
   end
+  def mastery_label(range, sat)
+    if !range.nil?
+      label = sat ? range.label_with_sat : range.range
+    else
+      label = 'Undefined Level'
+    end
+    label
+  end
+  def benchmarks(strand, range, standard)
+    benchmarks = []
+    if !range.nil? && !strand.nil? && !standard.nil?
+      benchmarks = standard.benchmarks_for_strand_range(strand, range)
+    end
+    benchmarks
+  end
+  def improvements(strand, range, standard)
+    improvements = []
+    if !range.nil? && !strand.nil? && !standard.nil?
+      improvements = standard.improvements_for_strand_range(strand, range)
+    end
+    improvements
+  end
 end

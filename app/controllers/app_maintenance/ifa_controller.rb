a08f26e07fafb9b91a463208ca833a0141031c80
Allen Krauskopf <alkrauskopf@gmail.com>
2017-07-23 08:51:18 -0600

    Benchmark Dashboard

--- app/controllers/app_maintenance/ifa_controller.rb
+++ app/controllers/app_maintenance/ifa_controller.rb
@@ -1056,7 +1056,8 @@ class AppMaintenance::IfaController < AppMaintenance::ApplicationController
     @types = @current_standard.act_bench_types
     @sources = ActMaster.national
     @bench_dashboard_hdr[0] = @types.map{|t| (t.abbrev + ' = ' + t.name.titleize)}.join(',  ')
-    @bench_total['all'] = 0
+    @bench_total['all'] = @current_subject.act_benches.for_standard(@current_standard).size
+    @bench_total['enabled'] = 0
     @active_levels.each do |level|
       @bench_total[level.range] = 0
     end
@@ -1068,7 +1069,7 @@ class AppMaintenance::IfaController < AppMaintenance::ApplicationController
         @bench_total[ls] = ActBench.for_level_strand(level, strand).size
         @bench_total[strand.abbrev] += @bench_total[ls]
         @bench_total[level.range] += @bench_total[ls]
-        @bench_total['all'] += @bench_total[ls]
+        @bench_total['enabled'] += @bench_total[ls]
         @bench_types_hdr[ls] = []
         @bench_types[ls] = []
         @types.each do |b_type|
